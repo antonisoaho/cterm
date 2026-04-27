af2ac01643426d91b64466f75783291eeb481f28
Anton Isoaho <anton.isoaho@hogia.se>
2026-04-27 11:23:47 +0200

    feat(nvim): entry point and base options

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+require('config.options')
+-- lazy + plugins arrive in Task 6
+-- keymaps arrive in Task 14
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,27 @@
+local opt = vim.opt
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = 'yes'
+opt.termguicolors = true
+opt.mouse = 'a'
+opt.clipboard = 'unnamedplus'
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.splitright = true
+opt.splitbelow = true
+opt.undofile = true
+opt.updatetime = 200
+opt.timeoutlen = 400
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.cursorline = true
+opt.wrap = false
+opt.fillchars = { eob = ' ' }
