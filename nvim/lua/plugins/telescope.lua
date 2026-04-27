97f39b9d89040ad8be5c2a0cfa9bba7a4ac6604e
Anton Isoaho <anton.isoaho@hogia.se>
2026-04-27 11:38:00 +0200

    feat(nvim): telescope fuzzy finder

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    cmd = 'Telescope',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+      { '<leader>fg', '<cmd>Telescope live_grep<cr>',  desc = 'Live grep' },
+      { '<leader>fb', '<cmd>Telescope buffers<cr>',    desc = 'Buffers' },
+      { '<leader>fh', '<cmd>Telescope help_tags<cr>',  desc = 'Help tags' },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = 'flex',
+        sorting_strategy = 'ascending',
+        layout_config = { prompt_position = 'top' },
+        path_display = { 'truncate' },
+      },
+    },
+  },
+}
